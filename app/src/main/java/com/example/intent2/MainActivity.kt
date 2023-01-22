6a96896dc0372ab5287c60567dcf7ad0238fdfec
dhermidaperez@danielcastelao.org <dhermidaperez@danielcastelao.org>
2023-01-22 20:20:46 +0100

    Segunda actividad (Operaciones acabadas)

--- app/src/main/java/com/example/intent2/MainActivity.kt
+++ app/src/main/java/com/example/intent2/MainActivity.kt
@@ -9,8 +9,12 @@ class MainActivity : AppCompatActivity() {
 
     // definir el requestCode
     val RESULTADO_UNO = 1
+    val RESULTADO_DOS = 2
     val NUMERO_UNO = (0..10).random()
     val NUMERO_DOS = (0..10).random()
+    val NUMERO_TRES = (0..10).random()
+    val NUMERO_CUATRO = (0..10).random()
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +24,18 @@ class MainActivity : AppCompatActivity() {
 
         gosecond.setOnClickListener{
             // Crea un Intent para iniciar la segunda actividad
+
             val intent = Intent(this, SecondActivity::class.java)
 
             // Añade nros al Intent
-            intent.putExtra("nro1", NUMERO_UNO)
-            intent.putExtra("nro2", NUMERO_DOS)
+            intent.putExtra ("nro1", NUMERO_UNO)
+            intent.putExtra ("nro2", NUMERO_DOS)
             // Inicia la segunda actividad
             startActivityForResult(intent, RESULTADO_UNO)
-
+            intent.putExtra ("nro1", NUMERO_TRES)
+            intent.putExtra ("nro2", NUMERO_CUATRO)
+            // Inicia la segunda actividad
+            startActivityForResult(intent, RESULTADO_DOS)
 
         }
 
--- app/src/main/java/com/example/intent2/SecondActivity.kt
+++ app/src/main/java/com/example/intent2/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.example.intent2
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -13,6 +15,32 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val intent = getIntent()
+
+        // Recoge el valor de tipo String pasado en el Intent
+        val nro1 = intent.getIntExtra("nro1",0)
+        val nro2 = intent.getIntExtra("nro2",0)
+
+
+        intent.putExtra("suma", nro1 + nro2);
+        // Debug
+        Log.d("MENSAJES", "actualizado intent")
+        // Configuro el result para que la Main lo tenga disponible
+        // Posibles resultados:
+        //    Activity.RESULT_OK
+        //    Activity.RESULT_CANCELED
+        setResult(Activity.RESULT_OK, intent);
+        // Debug
+        Log.d("MENSAJES", "actualizado resultado")
+        // cierro la activity
+        finish()
+
+
+
+
+
 
 
 
