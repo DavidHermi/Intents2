3bb3b2e6206ec0e6a44fd872c3b348c144ac14f2
dhermidaperez@danielcastelao.org <dhermidaperez@danielcastelao.org>
2023-01-22 23:09:32 +0100

    Acabadas Operaciones

--- app/src/main/java/com/example/intent2/MainActivity.kt
+++ app/src/main/java/com/example/intent2/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.intent2
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,32 +18,47 @@ class MainActivity : AppCompatActivity() {
     val NUMERO_CUATRO = (0..10).random()
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val gosecond = findViewById<Button>(R.id.boton2)
 
-        gosecond.setOnClickListener{
+        gosecond.setOnClickListener {
             // Crea un Intent para iniciar la segunda actividad
 
             val intent = Intent(this, SecondActivity::class.java)
 
             // Añade nros al Intent
-            intent.putExtra ("nro1", NUMERO_UNO)
-            intent.putExtra ("nro2", NUMERO_DOS)
+            intent.putExtra("nro1", NUMERO_UNO)
+            intent.putExtra("nro2", NUMERO_DOS)
             // Inicia la segunda actividad
             startActivityForResult(intent, RESULTADO_UNO)
-            intent.putExtra ("nro1", NUMERO_TRES)
-            intent.putExtra ("nro2", NUMERO_CUATRO)
-            // Inicia la segunda actividad
-            startActivityForResult(intent, RESULTADO_DOS)
 
         }
 
 
+    }
 
 
+    @Deprecated("Deprecated a partir de API 30")
+    // recoje el intent cuando hacemos setResult() en la SecondActivity
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Definimos el text view para mostrar el dato que nos manda la Second
+        val suma1 = findViewById<TextView>(R.id.suma1)
+
+
+        // Comprueba que el resultado es OK
+        if (resultCode != Activity.RESULT_OK || data == null) return
+        // Puedo distinguir diferentes acciones según el requestCode
+        when (requestCode) {
+            RESULTADO_UNO -> {
+                suma1.text = data.getIntExtra("suma", 0).toString()
+
+
+            }
+        }
 
     }
 }
+
